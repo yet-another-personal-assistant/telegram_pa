4532a5a76c9c9972866b83423f07c1098bb8f04b
aragaer <aragaer@gmail.com>
2017-07-12 23:45:51 +0300

    Can now accept some commands on stdin.

--- pa.py
+++ pa.py
@@ -1,33 +1,65 @@
 #!/usr/bin/env python3
+import asyncio
+import os
+import signal
+import sys
 import telepot
+
+from telepot.aio.loop import MessageLoop
 from time import sleep
 
+
 _OWNER_ID = None
+_bot = None
 
 
-def handle(msg):
-    print("Got something")
+@asyncio.coroutine
+def async_input(reader):
+    line = yield from reader.readline()
+    return line.decode('utf8').replace('\r', '').replace('\n', '')
+
+
+async def _handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
-    print("Channel type", chat_type)
-    if chat_id != _OWNER_ID:
+    if chat_id == _OWNER_ID:
+        await _bot.sendMessage(chat_id, "Даже не знаю, что ответить")
+    else:
         if chat_type == 'private':
-            bot.sendMessage(chat_id, 'Мы с вами не знакомы')
+            await _bot.sendMessage(chat_id, "Мы с вами не знакомы")
         else:
-            bot.sendMessage(chat_id, "Я куда-то не туда попалa")
-        bot.leaveChat(chat_id)
-        return
-    bot.sendMessage(chat_id, "Даже не знаю, что ответить")
+            await _bot.sendMessage(chat_id, "Я куда-то не туда попалa")
+        await _bot.leaveChat(chat_id)
 
 
-if __name__ == '__main__':
+def _read_config():
+    global _OWNER_ID, _bot
     with open("token.txt") as token_file:
         for line in token_file:
             key, value = line.strip().split()
             if key == 'TOKEN':
-                token = value
+                _bot = telepot.aio.Bot(value)
             elif key == 'OWNER':
                 _OWNER_ID = int(value)
-    bot = telepot.Bot(token)
-    bot.message_loop(handle)
+
+
+@asyncio.coroutine
+def main():
+    reader = asyncio.StreamReader()
+    reader_protocol = asyncio.StreamReaderProtocol(reader)
+    yield from asyncio.get_event_loop().connect_read_pipe(lambda: reader_protocol, sys.stdin)
     while True:
-        sleep(100)
+        command = (yield from async_input(reader)).strip()
+        print("Got command [{}]".format(command))
+        if command == 'stop':
+            break
+        yield from _bot.sendMessage(_OWNER_ID, "Мне тут пришла команда {}".format(command))
+    yield from _bot.sendMessage(_OWNER_ID, "Мне пора, чмоки!")
+
+
+if __name__ == '__main__':
+    _read_config()
+    loop = asyncio.get_event_loop()
+    for signame in (signal.SIGINT, signal.SIGTERM):
+        loop.add_signal_handler(signame, loop.stop)
+    loop.create_task(MessageLoop(_bot, _handle).run_forever())
+    loop.run_until_complete(main())
