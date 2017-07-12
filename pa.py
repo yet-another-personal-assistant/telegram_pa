288f72ce4ede788c19be4d54529b0cd77f7679aa
aragaer <aragaer@gmail.com>
2017-07-13 00:17:35 +0300

    Say goodbye on SIGINT or SIGTERM.

--- pa.py
+++ pa.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import asyncio
+import errno
 import os
 import signal
 import sys
@@ -11,6 +12,7 @@ from time import sleep
 
 _OWNER_ID = None
 _bot = None
+_FIFO = "pa_fifo"
 
 
 @asyncio.coroutine
@@ -41,25 +43,35 @@ def _read_config():
             elif key == 'OWNER':
                 _OWNER_ID = int(value)
 
-
 @asyncio.coroutine
 def main():
-    reader = asyncio.StreamReader()
-    reader_protocol = asyncio.StreamReaderProtocol(reader)
-    yield from asyncio.get_event_loop().connect_read_pipe(lambda: reader_protocol, sys.stdin)
-    while True:
-        command = (yield from async_input(reader)).strip()
-        print("Got command [{}]".format(command))
-        if command == 'stop':
-            break
-        yield from _bot.sendMessage(_OWNER_ID, "Мне тут пришла команда {}".format(command))
-    yield from _bot.sendMessage(_OWNER_ID, "Мне пора, чмоки!")
+    try:
+        reader = asyncio.StreamReader()
+        reader_protocol = asyncio.StreamReaderProtocol(reader)
+        yield from asyncio.get_event_loop().connect_read_pipe(lambda: reader_protocol, sys.stdin)
+        while True:
+            command = (yield from async_input(reader)).strip()
+            print("Got command [{}]".format(command))
+            if command == 'stop':
+                break
+            yield from _bot.sendMessage(_OWNER_ID, "Мне тут пришла команда {}".format(command))
+    except asyncio.CancelledError:
+        pass
 
 
 if __name__ == '__main__':
     _read_config()
+    try:
+        os.mkfifo(_FIFO)
+    except OSError as oe:
+        if oe.errno != errno.EEXIST:
+            raise
     loop = asyncio.get_event_loop()
+    main_task = asyncio.Task(main())
     for signame in (signal.SIGINT, signal.SIGTERM):
-        loop.add_signal_handler(signame, loop.stop)
+        loop.add_signal_handler(signame, main_task.cancel)
     loop.create_task(MessageLoop(_bot, _handle).run_forever())
-    loop.run_until_complete(main())
+    loop.run_until_complete(_bot.sendMessage(_OWNER_ID, "Так, я вернулась"))
+    loop.run_until_complete(main_task)
+    loop.run_until_complete(_bot.sendMessage(_OWNER_ID, "Мне пора, чмоки!"))
+    os.unlink(_FIFO)
