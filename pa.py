f7dc4710826ccb3d2b36681fea6220b8fb379179
aragaer <aragaer@gmail.com>
2017-07-13 10:02:25 +0300

    Death to globals!

--- pa.py
+++ pa.py
@@ -7,107 +7,105 @@ import telepot
 from telepot.aio.loop import MessageLoop
 
 
-_OWNER_ID = None
-_bot = None
 _UNIX = "/tmp/pa_socket"
-_writer = None
 
 
-async def send_msg_async(msg):
-    await _bot.sendMessage(_OWNER_ID, msg)
+class PersonalAssistant(object):
+    _OWNER_ID = None
+    _args = None
+    _bot = None
+    _server = None
+    _backend = None
 
-def send_msg_sync(msg):
-    asyncio.get_event_loop().create_task(send_msg_async(msg))
+    def __init__(self):
+        import argparse
+        parser = argparse.ArgumentParser(description="My Personal Assistant")
+        parser.add_argument("--no-greet", action='store_true', help="Skip greeting message")
+        parser.add_argument("--no-goodbye", action='store_true', help="Skip goodbye message")
+        self._args = parser.parse_args()
+        with open("token.txt") as token_file:
+            for line in token_file:
+                key, value = line.strip().split()
+                if key == 'TOKEN':
+                    self._bot = telepot.aio.Bot(value)
+                elif key == 'OWNER':
+                    self._OWNER_ID = int(value)
 
+    def send_msg_sync(self, msg):
+        asyncio.get_event_loop().create_task(self.send_msg_async(msg))
 
-def i_m_thinking():
-    send_msg_sync("Сейчас подумаю...")
+    async def send_msg_async(self, msg):
+        await self._bot.sendMessage(self._OWNER_ID, msg)
 
+    def i_m_thinking(self):
+        self.send_msg_sync("Сейчас подумаю...")
 
-async def _handle(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    if chat_id == _OWNER_ID:
-        if _writer is None:
-            await _bot.sendMessage(chat_id, "Ой, я сейчас по уши занята")
+    def accept_client(self, reader, writer):
+        task = asyncio.Task(self.handle_client(reader, writer))
+        def client_gone(task):
+            writer.close()
+            if writer == self._backend:
+                self._backend = None
+                _pa.send_msg_sync("Пойду дальше делами заниматься")
+        task.add_done_callback(client_gone)
+
+    async def handle_client(self, reader, writer):
+        while True:
+            data = await reader.readline()
+            if not data:
+                break
+            sdata = data.decode().strip()
+            if sdata:
+                await self._handle_local(sdata, reader, writer)
+
+    async def _handle_local(self, command, reader, writer):
+        if command == 'stop':
+            asyncio.get_event_loop().stop()
+        elif command.startswith('message:'):
+            message = command[8:].strip()
+            if message:
+                await self.send_msg_async(message)
+        elif command == 'register backend':
+            self._backend = writer
+            await self.send_msg_async("Вот, я слушаю")
+
+    async def _handle_remote(self, command):
+        if self._backend is None:
+            await self.send_msg_async("Ой, я сейчас по уши занята")
         else:
-            _writer.write("message:{}\n".format(msg['text']).encode())
-            asyncio.get_event_loop().call_later(1, i_m_thinking)
-    else:
-        if chat_type == 'private':
-            await _bot.sendMessage(chat_id, "Мы с вами не знакомы")
+            self._backend.write("message:{}\n".format(command).encode())
+            asyncio.get_event_loop().call_later(1, self.i_m_thinking)
+
+    async def _handle(self, msg):
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        if chat_id == self._OWNER_ID:
+            await self._handle_remote(msg['text'])
         else:
-            await _bot.sendMessage(chat_id, "Я куда-то не туда попалa")
-        await _bot.leaveChat(chat_id)
+            if chat_type == 'private':
+                await self._bot.sendMessage(chat_id, "Мы с вами не знакомы")
+            else:
+                await self._bot.sendMessage(chat_id, "Я куда-то не туда попалa")
+            await self._bot.leaveChat(chat_id)
 
+    async def task(self):
+        self._server = await asyncio.start_unix_server(self.accept_client, path=_UNIX)
+        await MessageLoop(self._bot, self._handle).run_forever()
 
-def _read_config():
-    global _OWNER_ID, _bot
-    with open("token.txt") as token_file:
-        for line in token_file:
-            key, value = line.strip().split()
-            if key == 'TOKEN':
-                _bot = telepot.aio.Bot(value)
-            elif key == 'OWNER':
-                _OWNER_ID = int(value)
-
-
-async def handle_local_command(command, reader, writer):
-    if command == 'stop':
-        asyncio.get_event_loop().stop()
-    elif command.startswith('message:'):
-        message = command[8:].strip()
-        if message:
-            await send_msg_async(message)
-    elif command == 'register backend':
-        global _writer
-        _writer = writer
-        await send_msg_async("Вот, я слушаю")
-
-
-async def handle_client(reader, writer):
-    while True:
-        data = await reader.readline()
-        if not data:
-            break
-        sdata = data.decode().strip()
-        if sdata:
-            await handle_local_command(sdata, reader, writer)
-
-
-def accept_client(reader, writer):
-    task = asyncio.Task(handle_client(reader, writer))
-    def client_gone(task):
-        writer.close()
-        global _writer
-        if writer == _writer:
-            _writer = None
-            send_msg_sync("Пойду дальше делами заниматься")
-    task.add_done_callback(client_gone)
-
-
-def _parse_args():
-    import argparse
-    parser = argparse.ArgumentParser(description="My Personal Assistant")
-    parser.add_argument("--no-greet", action='store_true', help="Skip greeting message")
-    parser.add_argument("--no-goodbye", action='store_true', help="Skip goodbye message")
-    return parser.parse_args()
+    def run(self):
+        loop = asyncio.get_event_loop()
+        for signame in (signal.SIGINT, signal.SIGTERM):
+            loop.add_signal_handler(signame, loop.stop)
+        if os.path.exists(_UNIX):
+            os.unlink(_UNIX)
+        if not self._args.no_greet:
+            self.send_msg_sync("Так, я вернулась")
+        loop.create_task(self.task())
+        loop.run_forever()
+        os.unlink(_UNIX)
+        self._server.close()
+        if not self._args.no_goodbye:
+            loop.run_until_complete(self.send_msg_async("Мне пора, чмоки!"))
 
 
 if __name__ == '__main__':
-    args = _parse_args()
-    _read_config()
-    loop = asyncio.get_event_loop()
-    for signame in (signal.SIGINT, signal.SIGTERM):
-        loop.add_signal_handler(signame, loop.stop)
-    loop.create_task(MessageLoop(_bot, _handle).run_forever())
-    if os.path.exists(_UNIX):
-        os.unlink(_UNIX)
-
-    loop.run_until_complete(asyncio.start_unix_server(accept_client, path=_UNIX))
-    if not args.no_greet:
-        send_msg_sync("Так, я вернулась")
-    loop.run_forever()
-    if not args.no_goodbye:
-        loop.run_until_complete(send_msg_async("Мне пора, чмоки!"))
-
-    os.unlink(_UNIX)
+    PersonalAssistant().run()
