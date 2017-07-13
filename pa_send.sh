58bbee0760375128e17bf9722fc62e086ae70400
aragaer <aragaer@gmail.com>
2017-07-13 12:14:19 +0300

    Fix pa_send.sh to handle paths with spaces.

--- pa_send.sh
+++ pa_send.sh
@@ -1,18 +1,19 @@
-#!/bin/sh +x
+#!/bin/sh
 
 message="$@"
-thisdir=$(readlink -f $(dirname $0))
+thisfile=$(readlink -f "$0")
 
 if [ ! -S /tmp/pa_socket ] ; then
     stop_cmd=stop
-    . "$thisdir"/.env/bin/activate
-    "$thisdir"/pa.py --no-greet --no-goodbye &
+    (cd $(dirname "$thisfile");
+     . .env/bin/activate;
+     ./pa.py --no-greet --no-goodbye &)
     while [ ! -S /tmp/pa_socket ] ; do
 	sleep 1
     done
 fi
 
-socat STDIO UNIX-CONNECT:/tmp/pa_socket <<EOF
+exec socat STDIO UNIX-CONNECT:/tmp/pa_socket <<EOF
 message:$message
 $stop_cmd
 EOF
