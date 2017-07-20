6b0c42ff8eda7b18ccc42dc4090c82fa581556ce
aragaer <aragaer@gmail.com>
2017-07-20 23:22:41 +0300

    Replace old spike code with somewhat better one.

--- pa.py
+++ pa.py
@@ -5,8 +5,17 @@ from spike.pa import PersonalAssistant
 
 if __name__ == '__main__':
     import argparse
+    import logging
+    import sys
     parser = argparse.ArgumentParser(description="My Personal Assistant")
     parser.add_argument("--no-greet", action='store_true', help="Skip greeting message")
     parser.add_argument("--no-goodbye", action='store_true', help="Skip goodbye message")
     parser.add_argument("--conf", default="token.txt", help="Configuration file")
+    parser.add_argument("--verbose", action='store_true', help="Configuration file")
+
+    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+    logging.getLogger('SM').setLevel(logging.DEBUG)
+    logging.getLogger('telepot').setLevel(logging.DEBUG)
+    logging.getLogger('asyncio').setLevel(logging.WARNING)
+
     PersonalAssistant(parser.parse_args()).run()
--- spike/pa.py
+++ spike/pa.py
@@ -1,27 +1,142 @@
 #!/usr/bin/env python3
+import logging
+import sys
+
 import asyncio
+import os
 import signal
 import telepot
 
+sys.path.append('.')
+asyncio.get_event_loop().set_debug(True)
+
+from assistant.state import StateMachine
+from functools import partial
 from telepot.aio.loop import MessageLoop
-
-from spike.session import Session
-
 
 _UNIX = "/tmp/pa_socket"
 
 
+class BackendConnection(object):
+
+    def __init__(self, reader, writer, session):
+        self._reader = reader
+        self._writer = writer
+        self._session = session
+        loop = asyncio.get_event_loop()
+        task = loop.create_task(self.run_forever())
+        task.add_done_callback(self.close)
+
+    async def run_forever(self):
+        while True:
+            data = await self._reader.readline()
+            if not data:
+                break
+            sdata = data.decode().strip()
+            if sdata:
+                await self._session.handle_local(sdata, self)
+
+    def write(self, message):
+        self._writer.write(message)
+
+    def close(self, task):
+        self._session.remove_client(self)
+        self._writer.close()
+
+
+class LocalSocket(object):
+
+    _server = None
+
+    def __init__(self, session, path):
+        self._session = session
+        self._path = path
+
+    async def start(self):
+        if os.path.exists(self._path):
+            os.unlink(self._path)
+        self._server = await asyncio.start_unix_server(
+            partial(BackendConnection, session=self._session),
+            path=self._path)
+
+    def stop(self):
+        os.unlink(self._path)
+        self._server.close()
+
+
+class Session(object):
+
+    _timer = None
+
+    def __init__(self, bot, chat_id, path, can_stop=False):
+        self._logger = logging.getLogger('telepot.{}'.format(chat_id))
+        self._bot = bot
+        self._backends = []
+        self._chat_id = chat_id
+        self._can_stop = can_stop
+        self._state_machine = StateMachine(self)
+        self._server = LocalSocket(self, path)
+
+    def start(self, event='start'):
+        self._state_machine.handle_event(event)
+
+    def start_server(self):
+        loop = asyncio.get_event_loop()
+        loop.create_task(self._server.start())
+
+    def stop(self, event='stop'):
+        self._state_machine.handle_event(event)
+        self._server.stop()
+
+    def remove_client(self, client):
+        if client in self._backends:
+            self._backends.remove(client)
+            if not self._backends:
+                self._state_machine.handle_event('backend gone')
+
+    def send_message(self, message):
+        self._logger.debug('sending to remote: "%s"', message)
+        loop = asyncio.get_event_loop()
+        loop.create_task(self._bot.sendMessage(self._chat_id, message))
+
+    async def handle_local(self, command, client):
+        self._logger.debug('got from local: "%s"', command)
+        if command == 'stop' and self._can_stop:
+            asyncio.get_event_loop().stop()
+        elif command.startswith('message:'):
+            message = command[8:].strip()
+            self._state_machine.handle_event('response', message)
+        elif command == 'register backend':
+            self._backends.insert(0, client)
+            if len(self._backends) == 1:
+                self._state_machine.handle_event('backend registered')
+        self._logger.debug('local message handled')
+
+    def send_to_backend(self, message):
+        self._logger.debug('sending to local: "%s"', message)
+        self._backends[0].write("message:{}\n".format(message).encode())
+
+    async def handle_remote(self, command):
+        self._state_machine.handle_event('message', command)
+
+    def start_timer(self, timeout):
+        loop = asyncio.get_event_loop()
+        if self._timer is None:
+            self._timer = loop.call_later(timeout, self._state_machine.handle_event, 'done')
+
+    def stop_timer(self):
+        if self._timer is not None:
+            self._timer.cancel()
+            self._timer = None
+
+
 class PersonalAssistant(object):
-    _args = None
-    _bot = None
-    _sessions = None
-    _friends = None
-    _ignored = None
 
     def __init__(self, args):
+        self._args = args
+        self._loop = asyncio.get_event_loop()
         self._friends = set()
         self._ignored = set()
-        self._args = args
         owner_id = None
         with open(self._args.conf) as token_file:
             for line in token_file:
@@ -38,45 +153,40 @@ class PersonalAssistant(object):
 
     async def _handle(self, msg):
         content_type, chat_type, chat_id = telepot.glance(msg)
-        if chat_id in self._sessions:
-            await self._sessions[chat_id]._handle_remote(msg['text'])
-        elif chat_id in self._friends:
+        if chat_id in self._friends and chat_id not in self._sessions:
             session = Session(self._bot, chat_id, _UNIX+str(chat_id))
             self._sessions[chat_id] = session
-            await session.start()
-            session.send_msg_sync("Ой, приветик")
-            await session._handle_remote(msg['text'])
+            session.start()
+
+        if chat_id in self._sessions and 'text' in msg:
+            self._loop.create_task(self._sessions[chat_id].handle_remote(msg['text']))
         else:
             if chat_type == 'private':
                 if chat_id not in self._ignored:
-                    await self._bot.sendMessage(chat_id, "Мы с вами не знакомы")
+                    self._loop.create_task(self._bot.sendMessage(chat_id, "Мы с вами не знакомы"))
                     self._ignored.add(chat_id)
             else:
-                await self._bot.sendMessage(chat_id, "Я куда-то не туда попалa")
-                await self._bot.leaveChat(chat_id)
-
-    async def task(self):
-        await MessageLoop(self._bot, self._handle).run_forever()
+                self._loop.create_task(self._bot.sendMessage(chat_id, "Я куда-то не туда попалa"))
+                self._loop.create_task(self._bot.leaveChat(chat_id))
 
     def run(self):
-        loop = asyncio.get_event_loop()
         for signame in (signal.SIGINT, signal.SIGTERM):
-            loop.add_signal_handler(signame, loop.stop)
+            self._loop.add_signal_handler(signame, self._loop.stop)
 
-        tasks = [s.start() for s in self._sessions.values()]
-        loop.run_until_complete(asyncio.gather(*tasks))
-        if not self._args.no_greet:
-            for session in self._sessions.values():
-                session.send_msg_sync("Так, я вернулась")
-
-        loop.create_task(self.task())
-        loop.run_forever()
-
+        start_event = 'silent start' if self._args.no_greet else 'owner start'
         for session in self._sessions.values():
-            session.stop()
-        if not self._args.no_goodbye:
-            tasks = [s.send_msg_async("Мне пора, чмоки!") for s in self._sessions.values()]
-            loop.run_until_complete(asyncio.gather(*tasks))
+            session.start(start_event)
+
+        message_loop = MessageLoop(self._bot, self._handle)
+        self._loop.create_task(message_loop.run_forever())
+        self._loop.run_forever()
+        message_loop.cancel()
+
+        stop_event = 'silent stop' if self._args.no_goodbye else 'stop'
+        for session in self._sessions.values():
+            session.stop(stop_event)
+        pending = asyncio.Task.all_tasks()
+        self._loop.run_until_complete(asyncio.gather(*pending))
 
         print("Ignored: {}".format(self._ignored))
 
@@ -85,5 +195,12 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description="My Personal Assistant")
     parser.add_argument("--conf", default="token.txt", help="Configuration file")
-    PersonalAssistant(parser.parse_args()).run()
-    PersonalAssistant().run()
+    parser.add_argument("--no-greet", action='store_true', help="Skip greeting message")
+    parser.add_argument("--no-goodbye", action='store_true', help="Skip goodbye message")
+    parser.add_argument("--verbose", action='store_true', help="Configuration file")
+    args = parser.parse_args()
+    if args.verbose:
+        logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+        logging.getLogger('SM').setLevel(logging.DEBUG)
+        logging.getLogger('asyncio').setLevel(logging.WARNING)
+    PersonalAssistant(args).run()
