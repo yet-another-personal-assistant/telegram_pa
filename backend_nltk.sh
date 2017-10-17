8886ee3eddbb9c8ee51d15d02a5ac8cfc6113d0e
aragaer <aragaer@gmail.com>
2017-10-17 22:22:47 +0300

    Remove complicated code.
    Leaving only the simple tg-stdio connector.

--- backend_nltk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-thisfile=$(readlink -f "$0")
-socket=$1
-test $socket || socket=/tmp/pa_socket
-(cd $(dirname "$thisfile")
- export NLTK_DATA=./nltk_data
- . .env/bin/activate
- socat UNIX-CONNECT:$socket EXEC:./backend_nltk.py,pty)
